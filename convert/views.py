f7eb5ac25a8adf9e414c4cac10a79f2d5804c8bc
Dmahi147 <mahic4148@gmail.com>
2020-10-08 12:18:42 +0530

    Updated the file upload and in file-upload.html page

--- convert/views.py
+++ convert/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.template import RequestContext
+
 from .models import Document
 from .forms import DocumentForm
 from django.contrib.auth.decorators import login_required
@@ -7,14 +9,18 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def model_form_upload(request):
+
+    # Handle file upload
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('users:home')
+
+            # Redirect to the document list after POST
+            return redirect('/pdf')
     else:
-        form = DocumentForm()
-    return render(request, 'app/file_upload.html', {
-        'form': form,
-        'file': True
-    })
+        form = DocumentForm() # A empty, unbound form
+
+    # Load documents for the list page
+    documents = Document.objects.all()
+    return render(request, 'app/file_upload.html', {'form': form, 'file': True, 'documents': documents})
